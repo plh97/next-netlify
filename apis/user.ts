40e83b343e74b521a2078cec726904c8e84363f7
plh97 <pengliheng111@gmail.com>
2023-10-19 13:24:16 +0800

    feat: support image render

--- apis/user.ts
+++ apis/user.ts
@@ -1,20 +1,5 @@
-import AxiosOrLocal from "@/utils/axiosOrLocal";
-
 export interface IViewer {
   avatarUrl: string;
   name: string;
   bio: string;
 }
-
-export const fetchUser = () =>
-  AxiosOrLocal<{ data: { viewer: IViewer } }>({
-    url: "/graphql",
-    method: "post",
-    data: {
-      query: `{
-        viewer {
-          name avatarUrl login bio url createdAt isHireable
-        }
-      }`,
-    },
-  });
